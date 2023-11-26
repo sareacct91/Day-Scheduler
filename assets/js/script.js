28f687c539179b822612fedd8a0c16ad2610c90a
Meow Roar <sareacct91@gmail.com>
2023-11-26 11:33:55 -0800

    added comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,10 @@
 let curHour = dayjs().hour();
 
 //#region Functions
-function checkTime() {
+// Function to check the current time and render grey, red, or green background to the time block
+// Using class: past, present, and future
+function renderBgColor() {
+  // i = 9 - 17, representing the hours of word day
   for (let i = 9; i <= 17; i++) {
     if (i < curHour) {
       $(`#hour-${i}`).addClass("past");
@@ -22,19 +25,19 @@ function checkTime() {
   }
 }
 
-// Render the schedules saved in localstorage
+// Render the schedules saved in localstorage and the correct background color
 function renderSchedule() {
   for (let i = 9; i <= 17; i++) {
     const text = localStorage.getItem(`hour-${i}`) || " ";
     $(`#hour-${i}`).children("textarea").val(text);
   }
-  checkTime();
+  renderBgColor();
 }
 
 function removeSchedule() {
   const id = $(this).parent().attr("id");
 
-  // Remove the schedule
+  // set the text to empty and delete the saved data in localstorage
   $(this).siblings("textarea").val('');
   localStorage.setItem(id, " ");
 }
@@ -47,6 +50,7 @@ function saveSchedule() {
   // save to localStorage
   localStorage.setItem(id, textInput);
 
+  // Show notification for save action for 2 seconds then remove the notification
   $('#confirmed').removeClass('d-none');
   setTimeout(() => {
     $('#confirmed').addClass('d-none');
@@ -66,12 +70,12 @@ function init() {
     curHour = dayjs().hour();
 
     // Update the time slot color
-    checkTime();
+    renderBgColor();
   }, 1000);
 
   // eventListener for save button click
   $(".container-lg").on("click", ".saveBtn", saveSchedule); 
-// eventListener for remove button click
+  // eventListener for remove button click
   $(".container-lg").on("click", ".removeBtn", removeSchedule); 
 }
 //#endregion functions
